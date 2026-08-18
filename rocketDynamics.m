3f8d623a0003e7e2c6c248c6f729a83330c36a20
Simon Rostad <60503048+simonrostad@users.noreply.github.com>
2026-08-18 16:07:30 -0600

    Updated to model Space X Super Heavy Booster + Variable mass flow

--- rocketDynamics.m
+++ rocketDynamics.m
@@ -1,22 +1,33 @@
 function dydt = rocketDynamics(t,y)
 
-g = 9.81;
+g = 9.81; %m/s^2
+p = 1.225; %kg/m^3
+Cd = 0.75; 
 
-mass = 1; %kg
+diameter = 9; %m
+a = pi * diameter^2 / 4; %m^2
 
-if t < 8
-    thrust = 15;
+burnTime = 166; %s
+propellantMass = 3.4e6; %kg
+massFlowRate = propellantMass/burnTime; %kg/s
+
+velocity = y(2); %m/s
+mass = y(3); %kg
+if t < burnTime
+    thrust = 73.5e6;
+    dmdt = -massFlowRate;
 else
     thrust = 0;
+    dmdt = 0;
 end
 
-velocity = y(2);
-
-acceleration = (thrust - mass*g)/mass;
+drag = 0.5*p*velocity*Cd*a*abs(velocity);
+acceleration = (thrust - mass*g - drag)/mass;
 
 dydt = [
     velocity;
     acceleration
+    dmdt
     ];
 
 end
